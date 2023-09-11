f6d263f2a7d46e8061bdeaf299ca87562105478f
Marie-Eve Picard (she/her) <77584086+me-pic@users.noreply.github.com>
2023-09-11 19:42:46 -0400

    fix bug

--- physprep/quality/time_sqi.py
+++ physprep/quality/time_sqi.py
@@ -367,6 +367,7 @@ def metrics_hr_sqi(intervals, metric="mean"):
             metric_rr = np.round(np.max(bpm), 4)
     except Exception:
         metric_rr = np.nan
+        print("Enter exception")
         traceback.print_exc()
         raise ValueError(f"Invalid metric: {metric}.")
     
