4082b108f4cc5b22450e01dc4865663ba39f6e1a
Marie-Eve Picard (she/her) <77584086+me-pic@users.noreply.github.com>
2023-09-17 13:19:33 -0400

    ADD data cleaning workflow

--- physprep/processing/clean.py
+++ physprep/processing/clean.py
@@ -4,17 +4,20 @@
 Neuromod cleaning utilities.
 """
 
-# from pathlib import Path
-
 import neurokit2 as nk
 import numpy as np
 from scipy import signal
 import pandas as pd
+from pathlib import Path
 from physprep import utils
 
 
-def preprocessing_workflow(data, metadata, workflow_strategy, save=True):
+def preprocessing_workflow(
+    data, metadata, workflow_strategy, outdir=None, filename=None, save=True
+):
     """
+    Apply the preprocessing workflow to a physiological recordings.
+
     Parameters
     ----------
     data : dataframe
@@ -23,6 +26,10 @@ def preprocessing_workflow(data, metadata, workflow_strategy, save=True):
         The metadata associated with the physiological recording.
     workflow_strategy : dict
         Dictionary containing the content of the workflow strategy.
+    outdir : str or pathlib.Path
+        Path to the directory where the preprocessed physiological data will be saved.
+    filename : str or pathlib.Path
+        Name of the file to save the preprocessed physiological data.
     save : bool
         Specify if the preprocessed signals should be saved.
         Default to True.
@@ -47,41 +54,80 @@ def preprocessing_workflow(data, metadata, workflow_strategy, save=True):
                 )
                 clean_signals.update({signal_type.id: clean})
             else:
-                print(f"No preprocessing strategy specified for {signal_type}. The preprocessing step will be skipped.")
-    
+                print(
+                    f"No preprocessing strategy specified for {signal_type}. "
+                    "The preprocessing step will be skipped."
+                )
 
     if save:
         # Save preprocessed signal
-        pass
+        if outdir is not None:
+            outdir = Path(outdir)
+        else:
+            print(
+                "WARNING! No output directory specified. Data will be saved in the "
+                f"current working directory: {Path.cwd()}"
+            )
+            outdir = Path.cwd()
+        # If signals have different lengths (i.e. different sampling rates), data will
+        # be saved in different files
+        vals = clean_signals.values()
+        ref_length = len(vals[0])
+        if all(len(item) == ref_length for item in vals):
+            # Save all signals in a single file
+            clean_signals = pd.DataFrame(clean_signals)
+            clean_signals.to_csv(
+                Path(outdir / filename).with_suffix(".tsv.gz"),
+                sep="\t",
+                index=False,
+                compression="gzip",
+            )
+        else:
+            for clean_signal in clean_signals:
+                signal_name = [
+                    name
+                    for name in workflow_strategy
+                    if workflow_strategy[name]["id"] == list(clean_signal.keys())[0]
+                ][0]
+                filename = filename.replace("physio", signal_name)
+                clean_signal = pd.DataFrame(clean_signal)
+                clean_signal.to_csv(
+                    Path(outdir / filename).with_suffix(".tsv.gz"),
+                    sep="\t",
+                    index=False,
+                    compression="gzip",
+                )
+        
+        return clean_signals
 
 
-def remove_padding(data, start_time=None, end_time=None, trigger_threshold=4):
+def remove_padding(data, start_time=None, end_time=None, trigger_threshold=None):
     """
     Remove padding in data if any.
 
     Parameters
     ----------
-    data : array or Series
-        The raw physiological signal.
+    data : dataframe
+        The raw physiological timeseries.
     start_time : float
-        The time at which the recording started (in seconds). 
-        Default to 0.
+        The time at which the recording started (in seconds). If specified, data will
+        be cropped before this time. Default to None.
     end_time : float
-        The time at which the recording ended (in seconds).
-        Default to None.
+        The time at which the recording ended (in seconds). If specified, data will
+        be cropped after this time. Default to None.
     trigger_threshold : float
         The threshold to use to detect the trigger. The data before the first
         detected trigger and after the last detected trigger will be removed.
         If `start_time` and/or `end_time` are specified, the trigger detection
         will be skipped and the data will be cropped based on the specified
-        times. Default to 4.
+        times. Default to None.
 
     Returns
     -------
     data : array or Series
         The raw physiological signal without padding.
     """
-    if start_time is None and end_time is None:
+    if trigger_threshold is not None:
         # Cropped the data based on the detected triggers
         trigger = data[data["TTL"] > trigger_threshold]
         start_time = list(trigger.index)[0]
@@ -92,7 +138,7 @@ def remove_padding(data, start_time=None, end_time=None, trigger_threshold=4):
             start_time = list(data[data["time"] == start_time].index)[0]
         if end_time is not None:
             end_time = list(data[data["time"] == end_time].index)[0]
-        
+
     data = data.loc[start_time:end_time].reset_index(drop=True)
 
     return data
@@ -108,8 +154,6 @@ def preprocess_signal(signal, preprocessing_strategy, sampling_rate=1000):
         The raw physiological signal.
     preprocessing_strategy : dict
         Dictionary containing the preprocessing steps to apply to the signal.
-    signal_type : str
-        The type of physiological signal. Can be 'ECG', 'PPG', 'RSP', 'EDA'.
     sampling_rate : float
         The sampling frequency of `signal` (in Hz, i.e., samples/second).
 
@@ -124,7 +168,7 @@ def preprocess_signal(signal, preprocessing_strategy, sampling_rate=1000):
     for step in preprocessing:
         if step["step"] == "filtering":
             if step["parameters"]["method"] == "notch":
-                pass
+                pass # TODO: implement notch filtering
             else:
                 signal = nk.signal_filter(
                     signal, sampling_rate=sampling_rate, **step["parameters"]
@@ -141,150 +185,7 @@ def preprocess_signal(signal, preprocessing_strategy, sampling_rate=1000):
                 "details, please refer to the Physprep documentation."
             )
 
-    # Create a dataframe with the raw and cleaned signal
-    # Dataframe containing the cleaned physiological signal.
-    # timeseries = pd.DataFrame(
-    #    {f"{signal_type}_Raw": raw, f"{signal_type}_Clean": signal}
-    #)
-
     return signal
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def neuromod_ppg_clean(ppg_signal, sampling_rate=10000.0, downsampling=None):
-    """
-    Clean a PPG signal.
-
-    Prepare raw PPG signal for peak detection with specified method.
-
-    Parameters
-    ----------
-    ppg_signal : list, array or Series
-        The raw PPG channel.
-    sampling_rate : float
-        The sampling frequency of `ppg_signal` (in Hz, i.e., samples/second).
-        Default to 10000.
-    downsampling : int
-        The desired sampling frequency (Hz). If None, the signal is not
-        resample.
-        Default to None.
-
-    Returns
-    -------
-    ppg_clean : array
-        Vector containing the cleaned PPG signal.
-    """
-    # Apply band pass filter
-    ppg_clean = nk.signal_filter(
-        ppg_signal, sampling_rate=sampling_rate, lowcut=0.5, highcut=8, order=3
-    )
-    # Downsample the signal if specified
-    if downsampling is not None:
-        ppg_clean = nk.signal_resample(
-            ppg_clean,
-            sampling_rate=sampling_rate,
-            desired_sampling_rate=downsampling,
-        )
-        ppg_signal = nk.signal_resample(
-            ppg_signal,
-            sampling_rate=sampling_rate,
-            desired_sampling_rate=downsampling,
-        )
-
-    return ppg_signal, ppg_clean
-
-
-# ======================================================================
-# Electrocardiogram (ECG)
-# =======================================================================
-
-
-def neuromod_ecg_clean(
-    ecg_signal,
-    sampling_rate=10000.0,
-    method="biopac",
-    me=False,
-    downsampling=None,
-):
-    """
-    Clean an ECG signal.
-
-    Prepare a raw ECG signal for R-peak detection with the specified method.
-
-    Parameters
-    ----------
-    ecg_signal : list, array or Series
-        The raw ECG channel.
-    sampling_rate : float
-        The sampling frequency of `ecg_signal` (in Hz, i.e., samples/second).
-        Default to 10000.
-    method : str
-        The processing pipeline to apply between 'biopac' and 'bottenhorn'.
-        Default to 'biopac'.
-    me : bool
-        Specify if the MRI sequence used was the multi-echo (True)
-        or the single-echo (False).
-        Default to False.
-    downsampling : int
-        The desired sampling frequency (Hz). If None, the signal is not
-        resample.
-        Default to None.
-
-    Returns
-    -------
-    ecg_clean : array
-        Vector containing the cleaned ECG signal.
-    """
-    if me:
-        tr = 2.65
-        mb = 2
-        slices = 70
-    else:
-        tr = 1.49
-        mb = 4
-        slices = 60
-
-    if method in ["biopac"]:
-        ecg_clean = _ecg_clean_biopac(ecg_signal, sampling_rate, tr=tr, slices=slices)
-    if method in ["bottenhorn", "bottenhorn2022"]:
-        # Apply comb band pass filter with Bottenhorn correction
-        print("... Applying the corrected comb band pass filter.")
-        ecg_clean = _ecg_clean_bottenhorn(
-            ecg_signal,
-            sampling_rate=sampling_rate,
-            tr=tr,
-            mb=mb,
-            slices=slices,
-        )
-    # Downsample the signal if specified
-    if downsampling is not None:
-        ecg_clean = nk.signal_resample(
-            ecg_clean,
-            sampling_rate=sampling_rate,
-            desired_sampling_rate=downsampling,
-        )
-        ecg_signal = nk.signal_resample(
-            ecg_signal,
-            sampling_rate=sampling_rate,
-            desired_sampling_rate=downsampling,
-        )
-
-    return ecg_signal, ecg_clean
 
 
 # =============================================================================
@@ -419,111 +320,6 @@ def _ecg_clean_bottenhorn(
     ecg_clean = nk.signal_filter(ecg_clean, sampling_rate=sampling_rate, highcut=40)
 
     return ecg_clean
-
-
-# =============================================================================
-# EDA
-# =============================================================================
-def neuromod_eda_clean(eda_signal, sampling_rate=10000.0, downsampling=None):
-    """
-    Multiband sequence gradient noise reduction.
-
-    Parameters
-    ----------
-    eda_signal : array
-        The EDA channel.
-    sampling_rate : float
-        The sampling frequency of `ecg_signal` (in Hz, i.e., samples/second).
-        Default to 10000.
-    downsampling : int
-        The desired sampling frequency (Hz). If None, the signal is not
-        resample.
-        Default to None.
-
-    Returns
-    -------
-    eda_clean : array
-        The cleaned EDA signal.
-
-    References
-    ----------
-    Bottenhorn, K. L., Salo, T., Riedel, M. C., Sutherland, M. T.,
-        Robinson, J. L., Musser, E. D., & Laird, A. R. (2021).
-        Denoising physiological data collected during multi-band,
-        multi-echo EPI sequences. bioRxiv, 2021-04.
-        https://doi.org/10.1101/2021.04.01.437293
-
-    See also
-    --------
-    https://neuropsychology.github.io/NeuroKit/functions/eda.html#preprocessing
-    """
-    # Low pass filtering at 3Hz, order=4
-    eda_clean = nk.eda_clean(eda_signal, sampling_rate=sampling_rate)
-    # Downsample the signal if specified
-    if downsampling is not None:
-        eda_clean = nk.signal_resample(
-            eda_clean,
-            sampling_rate=sampling_rate,
-            desired_sampling_rate=downsampling,
-        )
-        eda_signal = nk.signal_resample(
-            eda_signal,
-            sampling_rate=sampling_rate,
-            desired_sampling_rate=downsampling,
-        )
-
-    return eda_signal, eda_clean
-
-
-# =============================================================================
-# RSP
-# =============================================================================
-def neuromod_rsp_clean(rsp_signal, sampling_rate=10000.0, downsampling=None):
-    """
-    Clean a PPG signal.
-
-    Parameters
-    ----------
-    rsp_signal : list, array or Series
-        The raw RSP channel.
-    sampling_rate : float
-        The sampling frequency of `rsp_signal` (in Hz, i.e., samples/second).
-        Default to 10000.
-    downsampling : int
-        The desired sampling frequency (Hz). If None, the signal is not
-        resample.
-        Default to None.
-
-    Returns
-    -------
-    rsp_clean : array
-        Vector containing the cleaned PPG signal.
-
-    References
-    ----------
-    Khodadad, D., Nordebo, S., Müller, B., Waldmann, A., Yerworth, R.,
-        Becher, T., ... & Bayford, R. (2018). Optimized breath detection
-        algorithm in electrical impedance tomography. Physiological
-        measurement, 39(9), 094001.
-    """
-    # Apply bandpass filter
-    rsp_clean = nk.rsp_clean(
-        rsp_signal, sampling_rate=sampling_rate, method="khodadad2018"
-    )
-    # Downsample the signal if specified
-    if downsampling is not None:
-        rsp_clean = nk.signal_resample(
-            rsp_clean,
-            sampling_rate=sampling_rate,
-            desired_sampling_rate=downsampling,
-        )
-        rsp_signal = nk.signal_resample(
-            rsp_signal,
-            sampling_rate=sampling_rate,
-            desired_sampling_rate=downsampling,
-        )
-
-    return rsp_signal, rsp_clean
 
 
 # =============================================================================
--- physprep/tests/test_clean.py
+++ physprep/tests/test_clean.py
@@ -1,8 +1,46 @@
 """Test clean functions"""
 
 import numpy as np
+import pandas as pd
 
 from physprep.processing import clean
+
+
+def test_preprocessing_workflow():
+    pass
+
+
+def test_remove_padding():
+    # Simulate signal
+    simulated_data = {
+        "time": np.arange(-5, 10, 1),
+        "TTL": np.concatenate((np.zeros(5), np.full(5, 4.1), np.zeros(5)), axis=None),
+    }
+    simulated_data = pd.DataFrame(simulated_data)
+    # Remove padding based on the trigger threshold
+    cropped_data = clean.remove_padding(simulated_data, trigger_threshold=4)
+    assert len(cropped_data) == 5
+    # Remove padding based on `start_time`
+    cropped_data = clean.remove_padding(simulated_data, start_time=0)
+    assert len(cropped_data) == 10
+    # Remove padding based on `end_time`
+    cropped_data = clean.remove_padding(simulated_data, end_time=5)
+    assert len(cropped_data) == 11
+    # Remove padding based on `start_time` and `end_time`
+    cropped_data = clean.remove_padding(simulated_data, start_time=0, end_time=5)
+    assert len(cropped_data) == 6
+    # Remove padding when start_time = end_time = trigger_threshold = None
+    cropped_data = clean.remove_padding(simulated_data)
+    assert len(cropped_data) == len(simulated_data)
+    # Remove padding when start_time, end_time and trigger_threshold are not None
+    cropped_data = clean.remove_padding(
+        simulated_data, start_time=0, end_time=5, trigger_threshold=4
+    )
+    assert len(cropped_data) == 5
+
+
+def test_preprocess_signal():
+    pass
 
 
 def test_comb_band_stop():
